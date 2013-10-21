35712c275778e713ea10e863d75046057d49f4c2
happycoda <ypworkspace@gmail.com>
2013-10-21 11:07:19 +0400

    removing prefix option added

--- script.py
+++ script.py
@@ -2,11 +2,15 @@
 
 import os
 
-def rename(prefix):
-	cwd = os.getcwd()
-	listdir = os.listdir(cwd)
+def rename(prefix, flag, dir=None):
+	target_dir = os.getcwd() if dir is None else dir
+
+	listdir = os.listdir(target_dir)
 	full_path = None
 	for item in listdir:
-		full_path = cwd + '\\' + item
+		full_path = target_dir + '\\' + item
 		if os.path.isdir(full_path):
-			os.rename(full_path, cwd + '\\' + prefix + item)
+			if flag == 'add':
+				os.rename(full_path, target_dir + '\\' + prefix + item)
+			elif flag == 'remove':
+				os.rename(full_path, target_dir + '\\' + item[len(prefix):])
